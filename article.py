97bc489defd2440dc0b65f5d03424cb6e3796709
Eric Franson <tigfox@gmail.com>
2021-04-14 23:24:04 -0400

    add some argument handling

--- article.py
+++ article.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
+import sys
 import requests
 from bs4 import BeautifulSoup
+import argparse
 
-r = requests.get("https://www.washingtonpost.com/technology/2021/04/14/azimuth-san-bernardino-apple-iphone-fbi/")
+parser = argparse.ArgumentParser()
+parser.add_argument("source", help="a wapo article to pull, in the form of a URL")
+parser.add_argument("destination", help="filename to save article as. defaults to print to the screen")
+args = parser.parse_args()
+
+if not args.source:
+	print("You must provide a source URL")
+	sys.exit()
+
+r = requests.get(args.source)
 soup = BeautifulSoup(r.text, "html.parser")
 article = soup.find('div',{'class':'article-body'})
 article_text = article.find_all(text=True)
 
-print(soup.title)
-for line in article_text:
-    print(line)
+if not args.destination:
+	print("No destination provided, printing to screen")
+	print(soup.title)
+	for line in article_text:
+    	print(line)
+else:
+	# write some file handing stuff here and then print the text to it
